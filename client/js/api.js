98915adc8541c6be92b9825007493ef0b6b60693
Preston Pham <ppham@us.ibm.com>
2015-11-21 11:57:05 -0600

    scaffold api

---
+++ client/js/api.js
@@ -0,0 +1,22 @@
+default export const api = {
+
+  // callback(err, locations)
+  getAllLocations: function(callback) {
+
+  },
+
+  // callback(err, locations)
+  getLocation: function(id, callback) {
+
+  },
+
+  // callback(err, location)
+  createLocation: function(location, callback) {
+
+  },
+
+  // callback(err, locations)
+  addReport: function(id, callback) {
+
+  }
+};
