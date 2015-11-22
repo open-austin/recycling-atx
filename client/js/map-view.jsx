1a23089927436cdab5c27c1508d8c5faae4ead96
Preston Pham <ppham@us.ibm.com>
2015-11-22 13:21:21 -0600

    drop marker at current location

--- client/js/map-view.jsx
+++ client/js/map-view.jsx
@@ -9,40 +9,50 @@ export default class MapView extends React.Component {
     this.state = {};
   }
 
-  componentDidMount() {
+  initMap() {
     var map = L.map('map').setView([30.27, -97.746], 15);
-
-    function onAccuratePositionProgress (e) {
-    console.log(e.accuracy);
-    console.log(e.latlng);
-  }
-
-    function onAccuratePositionFound (e) {
-    console.log(e.accuracy);
-    console.log(e.latlng);
-  }
-
-    function onAccuratePositionError (e) {
-    console.log(e.message)
-  } 
-
-    map.on('accuratepositionprogress', onAccuratePositionProgress);
-    map.on('accuratepositionfound', onAccuratePositionFound);
-    map.on('accuratepositionerror', onAccuratePositionError);
-
-    map.findAccuratePosition({
-        maxWait: 15000, // defaults to 10000
-        desiredAccuracy: 30 // defaults to 20
-    });
-
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwaW5lZGEiLCJhIjoiY2loOWgybDhnMHR4eHUwa2xhOHRnYTJ3aiJ9.OLTnRf2Py_IXUBSfG8dbPQ', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
       id: 'mapineda.o7kgelpe',
       accessToken: 'pk.eyJ1IjoibWFwaW5lZGEiLCJhIjoiY2loOWgybDhnMHR4eHUwa2xhOHRnYTJ3aiJ9.OLTnRf2Py_IXUBSfG8dbPQ'
     }).addTo(map);
-
     this.map = map;
+  }
+
+  onAccuratePositionProgress (e) {
+    // TODO show in progress spinner?
+    console.log('Geolocate in progress');
+    // console.log(e.accuracy);
+    // console.log(e.latlng);
+  }
+
+  onAccuratePositionFound (e) {
+    const {lat, lng} = e.latlng;
+    // TODO Style this marker so users know where they are
+    // currently located
+    L.marker([lat, lng]).addTo(this.map);
+  }
+
+  onAccuratePositionError (e) {
+    // TODO show an error notification?
+    console.log('error', e);
+  }
+
+  zoomCurrentLocation() {
+    const map = this.map;
+    map.on('accuratepositionprogress', this.onAccuratePositionProgress.bind(this));
+    map.on('accuratepositionfound', this.onAccuratePositionFound.bind(this));
+    map.on('accuratepositionerror', this.onAccuratePositionError.bind(this));
+    map.findAccuratePosition({
+        maxWait: 15000, // defaults to 10000
+        desiredAccuracy: 30 // defaults to 20
+    });
+  }
+
+  componentDidMount() {
+    this.initMap();
+    this.zoomCurrentLocation();
   }
 
 
