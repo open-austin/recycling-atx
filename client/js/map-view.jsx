b14100acff826a3807fdc8a601e02ac4fe9d0025
mapineda Pineda <mapineda1222@gmail.com>
2015-11-22 11:50:05 -0600

    got accurate locations to work however pin is not dropping, yet consolelog is denoting accurate position

--- client/js/map-view.jsx
+++ client/js/map-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import findAccuratePosition from './accurateposition'
+
 export default class MapView extends React.Component {
 
   constructor(props) {
@@ -9,6 +11,29 @@ export default class MapView extends React.Component {
 
   componentDidMount() {
     var map = L.map('map').setView([30.27, -97.746], 15);
+
+    function onAccuratePositionProgress (e) {
+    console.log(e.accuracy);
+    console.log(e.latlng);
+  }
+
+    function onAccuratePositionFound (e) {
+    console.log(e.accuracy);
+    console.log(e.latlng);
+  }
+
+    function onAccuratePositionError (e) {
+    console.log(e.message)
+  } 
+
+    map.on('accuratepositionprogress', onAccuratePositionProgress);
+    map.on('accuratepositionfound', onAccuratePositionFound);
+    map.on('accuratepositionerror', onAccuratePositionError);
+
+    map.findAccuratePosition({
+        maxWait: 15000, // defaults to 10000
+        desiredAccuracy: 30 // defaults to 20
+    });
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwaW5lZGEiLCJhIjoiY2loOWgybDhnMHR4eHUwa2xhOHRnYTJ3aiJ9.OLTnRf2Py_IXUBSfG8dbPQ', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
