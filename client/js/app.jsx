5a5ed0e81d874bedae7dd933e27e49d207a61722
Preston Pham <ppham@us.ibm.com>
2015-11-22 12:52:10 -0600

    remove lat long for now

--- client/js/app.jsx
+++ client/js/app.jsx
@@ -10,9 +10,7 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       locations: [],
-      address: null,
-      lat: null,
-      long: null
+      address: null
     };
   }
 
