dc019d5dc22b0a637b4cb2767a49a309c7b473d9
mapineda Pineda <mapineda1222@gmail.com>
2016-02-03 22:20:00 -0600

    finilized footer design and links

--- client/js/components/footer.jsx
+++ client/js/components/footer.jsx
@@ -16,7 +16,7 @@ render() {
             <p className='footer-text'> An
               <a className='footer-link' href='http://open-austin.org' target='_blank'> Open-Austin</a> project |
               <a className='footer-link' href='http://github.com/open-austin/recycling-atx' target='_blank'> Github</a> |
-              <a className='footer-link' href='#' target='_blank'>About</a>
+              <a className='footer-link' href='#' target='_blank'> About</a>
             </p>
 
           </small>
