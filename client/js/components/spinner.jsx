42e978a6a41f284155a8e397165ecd8ba5166906
mapineda Pineda <mapineda1222@gmail.com>
2016-02-05 14:29:19 -0600

    update spinner app title

--- client/js/components/spinner.jsx
+++ client/js/components/spinner.jsx
@@ -12,6 +12,7 @@ export default class Spinner extends React.Component {
     return (
       <div className={className}>
         <div className="spinner-content">
+          <p>Recycle Please!</p>
           <p>Loading...</p>
         </div>
       </div>
